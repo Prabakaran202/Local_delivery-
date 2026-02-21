2d93b14a0dfbc1d79f8794bb4897f4dabd6a1ccf
prabakaran <140891635+Prabakaran202@users.noreply.github.com>
2026-02-21 21:40:54 +0530

    Update App.js

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return <Login />;
+  return <Register/>;
 }
 
 export default App;
